35224e8db5ad7654d625f2ed7880f581b82d2e00
LeeYanyan <102571900+CoderOFLee@users.noreply.github.com>
2024-03-27 22:53:07 +0800

    socket

--- lab1/mr/worker.go
+++ lab1/mr/worker.go
@@ -204,9 +204,9 @@ func worker(mapf func(string, string) []KeyValue,
 // usually returns true.
 // returns false if something goes wrong.
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	//sockname := coordinatorSock()
-	//c, err := rpc.DialHTTP("unix", sockname)
+	//c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
